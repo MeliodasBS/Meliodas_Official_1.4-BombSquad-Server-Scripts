9a9d4069fef2cdec72d80177ea6ff346664b8b3b
Meliodas♡ <95033655+MeliodasBS@users.noreply.github.com>
2021-11-26 13:09:32 +0530

    Update getPermissionsHashes.py

--- Data/scripts/getPermissionsHashes.py
+++ Data/scripts/getPermissionsHashes.py
@@ -1,13 +1,13 @@
 adminHashes = []
-vipHashes = ['pb-IF4HVXo6Aw==']
-banlist = {'pb-IF4dUhgMVw==': '\xee\x80\xb0Android54639809', 'pb-IF4pUmNePA==': '\xee\x80\xb0Android54917242', 'pb-IF5cVBFdVw==': '\xee\x80\xa0gauravlidDwarf78957'}
+vipHashes = []
+banlist = {}
 topperslist = []
 effectCustomers = {}
-customlist = {'pb-IF4SVEc-Lw==': '\xea\xa7\x81\xe0\xbc\x92\xf0\x92\x86\x9c\xf0\x9d\x95\xae\xf0\x9d\x96\x8d\xf0\x9d\x96\x86\xf0\x9d\x96\x92\xf0\x9d\x96\x95\xf0\x9d\x96\x8e\xf0\x9d\x96\x94\xf0\x9d\x96\x93\xf0\x92\x86\x9c\xe0\xbc\x92\xea\xa7\x82', 'pb-IF4HVXo6Aw==': 'HYPERCHOLESTEROLEMIA', 'pb-IF42VRUvDg==': '\xea\xa7\x81\xe0\xa6\x94\xe0\xa7\xa3\xe2\x98\xac\xe2\x9c\x9e\xf0\x9d\x93\x93\xf0\x9d\x96\x94\xf0\x9d\x96\x93\xe2\x9c\x9e\xe2\x98\xac\xe0\xa6\x94\xe0\xa7\xa3\xea\xa7\x82', 'pb-IF4yVVUlMw==': u'\ue048LEGEND_NEVER_DIES\ue048'}
-ownerHashes = ['pb-IF5VUhEABA==','pb-IF4gVWUaUQ==']
+customlist = {}
+ownerHashes = []
 coownerHashes= []
 surroundingObjectEffect = []
-sparkEffect = ['pb-IF5VUhEABA==','pb-IF4gVWUaUQ==']
+sparkEffect = []
 smokeEffect = []
 scorchEffect = [] 
 distortionEffect = []
