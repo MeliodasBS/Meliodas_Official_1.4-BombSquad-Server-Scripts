1f8c40616c280679ac6b2720cce61bebbdafca1d
Meliodas♡ <95033655+MeliodasBS@users.noreply.github.com>
2021-11-26 13:19:03 +0530

    Update Sender.py

--- Data/scripts/Sender.py
+++ Data/scripts/Sender.py
@@ -5,7 +5,7 @@ import bsUtils
 from bsUtils import *
 import random
 
-Messages = ['Rank 1 will get Admin','Rank 2 And 3 will get Vip','Join Our Discord Server By Clicking on The Stats Button','For Any Complaints Join Our Discord Server','Top 10 players will only Get Tag','contact Zyro In discord For Any Queries','join our discord Server Now For Giveaways','You will Not get admin or Vip if You not Joined Our Discord Server even if you are in Top 3']
+Messages = ['Rank 1 will get Admin','Rank 2 And 3 will get Vip']
 
 DelayLength = 65 #Should Be In Seconds
 
